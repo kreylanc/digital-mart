4d0927878647edc0eb68caee5318f78e10d88700
Kreylanc <devjanpun@gmail.com>
2023-12-30 12:14:51 +0000

    feat: config nodemailer w/payload and resend

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,10 +1,23 @@
 import dotenv from "dotenv";
 import path from "path";
-import payload from "payload";
+import payload, { Payload } from "payload";
 import type { InitOptions } from "payload/config";
+import nodemailer from "nodemailer";
+
 // get path of the .env file
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
+});
+
+// Setuping nodemailer to send emails via resend
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 // cache data
@@ -21,7 +34,9 @@ interface Args {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+export const getPayloadClient = async ({
+  initOptions,
+}: Args = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error("PAYLOAD_SECRET is missing");
   }
@@ -35,8 +50,13 @@ export const getPayloadClient = async ({ initOptions }: Args = {}) => {
   if (!cached.promise) {
     // initialie payload
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "noreply@frontendroadmap.com",
+        fromName: "Digital Hippo",
+      },
       secret: process.env.PAYLOAD_SECRET,
-      local: initOptions?.express ? false : true, // start payload in local mode
+      local: initOptions?.express ? false : true, // start payload in local mode if there's express server
       ...(initOptions || {}),
     });
   }
