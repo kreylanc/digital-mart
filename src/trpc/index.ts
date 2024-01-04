422dcb1a760a8ab1074fa04c751171515e526a67
Kreylanc <devjanpun@gmail.com>
2024-01-04 14:41:27 +0000

    API: infinite query for products

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,61 @@
+import { getPayloadClient } from "./../get-payload";
+import { z } from "zod";
 import { authRouter } from "./routers/auth";
-import { router } from "./trpc";
+import { publicProcedure, router } from "./trpc";
+import { QueryValidator } from "@/lib/validators/query-validator";
 
 // Defining api routers
 export const appRouter = router({
   auth: authRouter,
+  getInfiniteProcedure: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100),
+        cursor: z.number().nullish(), // a pointer determining the page number for pagination
+        query: QueryValidator,
+      })
+    )
+    .query(async ({ input }) => {
+      // desctructuring input
+      const { query, cursor } = input;
+
+      const { sort, limit, ...queryOpts } = query; // desctructure query
+
+      /*
+       * Parsing the category property to be valid for passing it in Payload where query
+       */
+      const parsedQueryOpts: Record<string, { equals: string }> = {};
+
+      Object.entries(queryOpts).forEach(([key, value]) => {
+        parsedQueryOpts[key] = {
+          equals: value,
+        };
+      });
+
+      const page = cursor || 1;
+
+      const payload = await getPayloadClient();
+
+      const {
+        docs: products,
+        hasNextPage,
+        nextPage,
+      } = await payload.find({
+        collection: "products",
+        where: {
+          status: {
+            equals: "approved",
+          },
+          ...parsedQueryOpts,
+        },
+        sort,
+        limit,
+        depth: 1,
+        page,
+      });
+
+      return { products, nextPage: hasNextPage ? nextPage : null };
+    }),
 });
 
 // Export the type of appRouter
